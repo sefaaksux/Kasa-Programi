451b5a2c20011d576ade3df862a24912fe730939
sefaaksu <sefaaksusefa@gmail.com>
2024-03-10 15:31:47 +0300

    Project name renamed

--- KasaProgramı/Properties/AssemblyInfo.cs
+++ KasaProgramı/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AksuHaliEvi")]
+[assembly: AssemblyProduct("KasaProgramı")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
